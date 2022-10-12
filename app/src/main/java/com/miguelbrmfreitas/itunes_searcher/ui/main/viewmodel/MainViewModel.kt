e3e76b34ca7d9b13eac4c3d2389197318718f4b8
MiguelbrmFreitas <miguelbrmfreitas@gmail.com>
2022-10-12 04:50:32 -0300

    - Added query params for getSearchResults GET method

--- app/src/main/java/com/miguelbrmfreitas/itunes_searcher/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/miguelbrmfreitas/itunes_searcher/ui/main/viewmodel/MainViewModel.kt
@@ -1,13 +1,22 @@
 package com.miguelbrmfreitas.itunes_searcher.ui.main.viewmodel
 
+import androidx.lifecycle.viewModelScope
 import com.miguelbrmfreitas.domain.usecases.GetResultsUseCase
 import com.miguelbrmfreitas.itunes_searcher.ui.base.BaseViewModel
+import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val getResultsUseCase: GetResultsUseCase,
     val mainViewModelState: MainViewModelState
 ) : BaseViewModel() {
+
     override fun startView() {
 
+    }
+
+    private fun getSearchResults(searchTerm: String) {
+        viewModelScope.launch {
+
+        }
     }
 }
--- data/src/main/java/com/miguelbrmfreitas/data/remote/ItunesSearcherService.kt
+++ data/src/main/java/com/miguelbrmfreitas/data/remote/ItunesSearcherService.kt
@@ -2,9 +2,15 @@ package com.miguelbrmfreitas.data.remote
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ItunesSearcherService
 {
     @GET("search")
-    suspend fun getSearchResults(): Response<ResultsApiResponse>
+    suspend fun getSearchResults(
+        @Query("term") searchTerm: String,
+        @Query("media") mediaType: String,
+        @Query("entity") entity: String,
+        @Query("limit") limit: Int
+    ): Response<ResultsApiResponse>
 }
