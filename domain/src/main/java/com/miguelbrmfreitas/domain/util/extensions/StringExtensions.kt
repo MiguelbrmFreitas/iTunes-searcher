15338168089bb27476183d2b0faed4d4c2da8905
MiguelbrmFreitas <miguelbrmfreitas@gmail.com>
2022-10-18 03:34:17 -0300

    - Implemented String to Date converter

--- domain/src/main/java/com/miguelbrmfreitas/domain/util/extensions/StringExtensions.kt
+++ domain/src/main/java/com/miguelbrmfreitas/domain/util/extensions/StringExtensions.kt
@@ -1,11 +1,13 @@
 package com.miguelbrmfreitas.domain.util.extensions
 
-import java.util.Date
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun String.replaceSpaces(): String {
     return replace(' ', '+')
 }
 
 fun String.toDate(): Date {
-    return Date()
+    val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
+    return format.parse(this)
 }
